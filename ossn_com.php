d8a2dd5d6c1cdfb36c19c440cded4fc29471b366
Arsalan Shah <manactric@gmail.com>
2020-02-12 02:49:39 +0500

    1.0

---
+++ ossn_com.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   (softlab24.com).ossn
+ * @author    OSSN Core Team <info@softlab24.com>
+ * @copyright (C) SOFTLAB24 LIMITED
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+define('MessageTyping', ossn_route()->com . 'MessageTyping/');
+ossn_register_class(array(
+		'MessageTyping' => MessageTyping . 'classes/MessageTyping.php'
+));
+function message_typing_init() {
+		ossn_extend_view('css/ossn.default', 'messagetyping/css');
+		ossn_extend_view('js/opensource.socialnetwork', 'messagetyping/js');
+		
+		ossn_extend_view('js/OssnChat.Boot', 'messagetyping/check_status');
+		if(ossn_isLoggedin()) {
+				ossn_register_action('message/typing/status/save', MessageTyping . 'actions/status.php');
+		}
+}
+ossn_register_callback('ossn', 'init', 'message_typing_init');
