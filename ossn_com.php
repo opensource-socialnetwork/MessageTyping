ec16ec3072351e7d9fe7c47af63329fd159ffbf1
Arsalan Shah <manactric@gmail.com>
2020-04-16 16:52:03 +0500

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -19,6 +19,22 @@ function message_typing_init() {
 		ossn_extend_view('js/OssnChat.Boot', 'messagetyping/check_status');
 		if(ossn_isLoggedin()) {
 				ossn_register_action('message/typing/status/save', MessageTyping . 'actions/status.php');
+				ossn_register_callback('user', 'delete', 'message_typing_user_delete');
+		}
+}
+function message_typing_user_delete($event, $type, $params) {
+		if(!empty($params['entity']->guid)) {
+				$delete = new MessageTyping;
+				$list   = $delete->searchAnnotation(array(
+						'type' => 'messagetypingstatus',
+						'wheres' => "(a.owner_guid={$params['entity']->guid} OR a.subject_guid={$params['entity']->guid})",
+						'page_limit' => false
+				));
+				if($list) {
+						foreach($list as $annotation) {
+								$annotation->deleteAnnotation();
+						}
+				}
 		}
 }
 ossn_register_callback('ossn', 'init', 'message_typing_init');
